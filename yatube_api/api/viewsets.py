92554a6a1830a98c2e7d11ed0121a702f2e3baca
goldreborn <yakov.kuznetsov98@yandex.ru>
2024-03-09 15:55:10 +0300

    update

--- yatube_api/api/viewsets.py
+++ yatube_api/api/viewsets.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import Serializer
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from django.db.models.query import QuerySet
 
 from posts.models import Follow
 
@@ -11,5 +12,5 @@ class FolowCustom(CreateModelMixin, ListModelMixin, GenericViewSet):
     def perform_create(self, serializer: Serializer) -> None:
         serializer.save(user=self.request.user)
 
-    def get_queryset(self) -> list[Follow]:
+    def get_queryset(self) -> QuerySet:
         return self.request.user.following.all()
